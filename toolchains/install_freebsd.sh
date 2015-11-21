d38b9d557170e71026c790e6c02bb7ecaa4098a5
Oliver Beddows <oliver.beddows@googlemail.com>
2015-11-21 16:55:28 +0000

    Adding FreeBSD toolchain.

---
+++ toolchains/install_freebsd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+. "$(pwd)/../shared.sh"
+
+if [ ! $(uname -s) = "FreeBSD" ]; then
+  echo "Invalid operating system. Aborting."
+  exit 1
+fi
+
+CMDS=("pkg")
+check_cmds CMDS[@]
+
+sudo pkg install -y autoconf automake curl gcc gmake node012 npm012 python security/ca_root_nss wget unzip zip
+
+if [ ! -e "/usr/local/bin/make" ] && [ -e "/usr/bin/make" ]; then
+  sudo mv /usr/bin/make /usr/bin/make.moved
+  sudo ln -s /usr/local/bin/gmake /usr/local/bin/make
+fi
+
+if [ ! -e "/usr/local/bin/gcc" ] && [ -e "/usr/local/bin/gcc48" ]; then
+  sudo ln -s /usr/local/bin/gcc48 /usr/local/bin/gcc
+fi
+
+if [ ! -e "/usr/local/bin/g++" ] && [ -e "/usr/local/bin/g++48" ]; then
+  sudo ln -s /usr/local/bin/g++48 /usr/local/bin/g++
+fi
