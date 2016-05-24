d5ba5616c0b8e914899fa2f1546fba1ac15cfc0c
Olivier Beddows <olivier@lisk.io>
2016-05-24 16:49:58 +0000

    Changing network.

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ JOBS="2"
 
 LISK_DIR=$VERSION
 LISK_FILE="$VERSION.tar.gz"
-LISK_NETWORK="test"
+LISK_NETWORK="main"
 LISK_URL="http://downloads.lisk.io/lisk/$LISK_NETWORK/$VERSION/$LISK_FILE"
 LISK_CONFIG=""
 
