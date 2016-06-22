54d30d6b834d1fbc0530366b1fff8a7aee254d86
Isabello <dell.isabella@gmail.com>
2016-06-21 23:54:24 -0500

    Update lisk.sh

--- scripts/lisk.sh
+++ scripts/lisk.sh
@@ -19,7 +19,7 @@ NETWORK="test"
 LISK_CONFIG=${2:-config.json}
 CONFIG_NAME=`echo $LISK_CONFIG | cut -f 1 -d '.'`
 
-DB_NAME=`grep "database" $LISK_CONFIG | awk -d'"' '{print $4}' | cut -f 2 -d '"'`
+DB_NAME=`grep "database" $LISK_CONFIG | awk --field-separator='"' '{print $4}' `
 DB_USER=$USER
 DB_PASS="password"
 DB_DATA="$(pwd)/pgsql/data"
