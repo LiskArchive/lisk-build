3fb4fe1f9bae6682f2c9e69e472c69e31aabd845
Isabello <dell.isabella@gmail.com>
2016-08-25 17:45:02 -0500

    Parameterized Source DB name. renamed -c flag

--- scripts/lisk_snapshot.sh
+++ scripts/lisk_snapshot.sh
@@ -8,30 +8,34 @@ cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 . "$(pwd)/env.sh"
 
 SNAPSHOT_CONFIG="snapshot.json"
-LISK_CONFIG="config.json"
-BACKUP_LOCATION="./backups"
+TARGET_DB_NAME="$(grep "database" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
 LOG_LOCATION="$(grep "logFileName" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
+
+LISK_CONFIG="config.json"
+SOURCE_DB_NAME="$(grep "database" $LISK_CONFIG | cut -f 4 -d '"')"
+
+BACKUP_LOCATION="./backups"
+
 DAYS_TO_KEEP="7"
-DB_NAME="$(grep "database" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
 
 parse_option() {
 
  OPTIND=1
- while getopts :s:c:b:d: opt;
+ while getopts :s:t:b:d: opt;
  do
    case $opt in
-   s) if [ -f $OPTARG ]; then
+   t) if [ -f $OPTARG ]; then
           SNAPSHOT_CONFIG=$OPTARG
-          DB_NAME="$(grep "database" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
+          TARGET_DB_NAME="$(grep "database" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
           LOG_LOCATION="$(grep "logFileName" $SNAPSHOT_CONFIG | cut -f 4 -d '"')"
       else
           echo "Config.json for snapshot not found. Please verify the file exists and try again."
           exit 1
       fi ;;
 
-   c) if [ -f $OPTARG ]; then
+   s) if [ -f $OPTARG ]; then
           LISK_CONFIG=$OPTARG
-          DB_NAME="$(grep "database" $LISK_CONFIG | cut -f 4 -d '"')"
+          SOURCE_DB_NAME="$(grep "database" $LISK_CONFIG | cut -f 4 -d '"')"
       else
           echo "Config.json not found. Please verify the file exists and try again."
           exit 1
@@ -64,11 +68,12 @@ parse_option() {
 
 usage() {
 
-  echo "Usage: $0 [-s <snapshot.json>] [-c <config.json>] [-b <backup directory>] [-d <days to keep>]"
-  echo " -s <snapshot.json>        -- config.json to use for validation"
-  echo " -c <config.json>          -- config.json to create target database"
+  echo "Usage: $0 [-s <config.json>] [-t <snapshot.json>] [-b <backup directory>] [-d <days to keep>]"
+  echo " -t <snapshot.json>        -- config.json to use for validation"
+  echo " -s <config.json>          -- config.json to create target database"
   echo " -b <backup directory>     -- backup direcory"
   echo " -d <days to keep>         -- Days to keep backups"
+
 }
 
 parse_option "$@"
@@ -82,21 +87,13 @@ mkdir -p $BACKUP_LOCATION  &> /dev/null
 echo -e "\nClearing old snapshots on disk"
 find $BACKUP_LOCATION -name lisk_backup* -mtime +$DAYS_TO_KEEP -exec rm {} \;
 
-if [ "$(grep "da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba" $LISK_CONFIG )" ];then
-  NETWORK="test"
-elif [ "$(grep "ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511" $LISK_CONFIG )" ];then
-  NETWORK="main"
-else
-  NETWORK="local"
-fi
-
 echo -e "\nClearing old snapshot instance"
 
-dropdb --if-exists "$DB_NAME" &> /dev/null
-createdb "$DB_NAME" &> /dev/null
+dropdb --if-exists "$TARGET_DB_NAME" &> /dev/null
+createdb "$TARGET_DB_NAME" &> /dev/null
 
 echo -e "\nExporting active database to snapshot instance"
-pg_dump lisk_$NETWORK | psql "$DB_NAME" &> /dev/null
+pg_dump $SOURCE_DB_NAME | psql "$TARGET_DB_NAME" &> /dev/null
 
 echo -e "\nClearing old log files"
 cat /dev/null > $LOG_LOCATION
@@ -116,6 +113,6 @@ psql -d lisk_snapshot -c 'delete from peers;'  &> /dev/null
 HEIGHT="$(psql -d lisk_snapshot -t -c 'select height from blocks order by height desc limit 1;')"
 
 echo -e "\nDumping snapshot"
-pg_dump -O "$DB_NAME" | gzip > $BACKUP_LOCATION/lisk_backup-"$(echo $HEIGHT)".gz
+pg_dump -O "$TARGET_DB_NAME" | gzip > $BACKUP_LOCATION/lisk_backup-"$(echo $HEIGHT)".gz
 
 echo -e "\nSnapshot Complete"
