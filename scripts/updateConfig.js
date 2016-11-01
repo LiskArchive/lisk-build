e79a3f75ad05054d1e6c716a44f898ab0de82aca
Isabello <dell.isabella@gmail.com>
2016-11-01 16:37:47 -0500

    Refactored to use extend to merge instead of 1:1 copy

--- scripts/updateConfig.js
+++ scripts/updateConfig.js
@@ -1,49 +1,31 @@
 var program = require('commander');
 var fs = require('fs');
 var path = require('path');
+var extend = require('extend');
 
 program
-  .version('1.0.0')
-  .option('-o, --old <path>', 'Old config.json')
-  .option('-n, --new <path>', 'New config.json')
-  .parse(process.argv);
+        .version('0.1.0')
+        .option('-o, --old <path>', 'Old config.json')
+        .option('-n, --new <path>', 'New config.json')
+        .parse(process.argv);
 
-var oldConfig;
+var oldConfig, newConfig;
 
 if (program.old) {
-        oldConfig = require(path.resolve(program.old));
+oldConfig = JSON.parse(fs.readFileSync(program.old, 'utf8'));
+delete oldConfig.version;
 } else {
-        console.log('Old config.json not found, please populate entries manually');
+        console.log('Old config.json not provided, please populate entries manually');
         process.exit(1);
 }
 
+if (program.new) {
+        newConfig = JSON.parse(fs.readFileSync(program.new, 'utf8'));
+        newConfig = extend(true, {}, newConfig, oldConfig);
 
-fs.readFile(program.new, 'utf8', function (err, data)  {
-  if (err) {
-    throw err;
-  }
-  var config = JSON.parse(data);
-  config.port = oldConfig.port;
-  config.address = oldConfig.address;
-  config.fileLogLevel = oldConfig.fileLogLevel;
-  config.logFileName = oldConfig.logFileName;
-  config.consoleLogLevel = oldConfig.consoleLogLevel;
-  config.trustProxy = oldConfig.trustProxy;
-  config.api.access.whiteList = oldConfig.api.access.whiteList;
-  config.forging.secret = oldConfig.forging.secret;
-  config.forging.access.whitelist = oldConfig.forging.access.whitelist;
-  config.ssl.enabled = oldConfig.ssl.enabled;
-  config.ssl.options.port = oldConfig.ssl.options.port;
-  config.ssl.options.address = oldConfig.ssl.options.address;
-  config.ssl.options.key = oldConfig.ssl.options.key;
-  config.ssl.options.cert = oldConfig.ssl.options.cert;
-  config.dapp.masterrequired = oldConfig.dapp.masterrequired;
-  config.dapp.masterpassword = oldConfig.dapp.masterpassword;
-  config.dapp.autoexec = oldConfig.dapp.autoexec;
-
-  fs.writeFile(program.new, JSON.stringify(config, null, 4), function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-});
+        fs.writeFile(program.new, JSON.stringify(newConfig, null, 4), function (err) {
+                if (err) {
+                        throw err;
+                }
+        });
+}
