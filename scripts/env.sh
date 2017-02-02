46a1d6856200d1f7512fbe6cfee9947c387bd97e
Isabello <isabella@lisk.io>
2017-02-02 03:03:07 -0800

    Correct SC2164 and SC2155 in env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit 2
 
+#shellcheck disable=SC2155
+#We dont care about return code here
 export PATH="$(pwd)/bin:$(pwd)/pgsql/bin:$PATH"
 export LD_LIBRARY_PATH="$(pwd)/pgsql/lib:$LD_LIBRARY_PATH"
