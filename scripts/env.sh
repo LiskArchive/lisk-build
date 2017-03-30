6f32b45587e564eede439f550e032f05cb186f90
Nerigal <lisk-git@l2web.ca>
2017-03-29 22:07:20 -0400

    fixed syntax error
    fixed syntax error

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,7 +7,7 @@ cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit 2
 export PATH="$(pwd)/bin:$(pwd)/pgsql/bin:$PATH"
 # We dont care about return code here
 # shellcheck disable=SC2155
-export LD_LIBRARY_PATH="$(pwd)/pgsql/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="${pwd}/pgsql/lib:$LD_LIBRARY_PATH"
 
 # Detect if lisk run under security system account to fake home dir to fix security issue with pm2 
 if ! [[ -d "/home/$USER" ]];then 
