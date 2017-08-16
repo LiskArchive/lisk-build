6e453b60996bb225ba860ada52e22d3923e57244
isabello <isabella@lightcurve.io>
2017-08-16 17:04:27 +0200

    Comment and whitespace adjustments

--- scripts/lisk_bridge.sh
+++ scripts/lisk_bridge.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-# Lisk bridge
-# Connects source and target versions of Lisk
-# In order to migrate gracefully between
-# Protocol changes.
+# LiskHQ/lisk-bridge
+#
+# Connects source and target versions of Lisk in order to migrate
+# gracefully between protocol changes.
+#
+# Copyright (C) 2017 Lisk Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################
 
 # Declare working variables
 TARGET_HEIGHT="3513100"
 BRIDGE_HOME="$(pwd)"
 BRIDGE_NETWORK="main"
 LISK_HOME="$HOME/lisk-main"
-
-
 
 # Reads in programatic variables if not the default
 parseOption() {
@@ -25,11 +39,8 @@ parseOption() {
 	 done
 }
 
-parseOption # Call parse option
-
 # This function harvests the configuation data from the source
-# installation for use in this program
-
+# installation for an automated cutover.
 extractConfig() {
   PM2_CONFIG="$LISK_HOME/etc/pm2-lisk.json"
   LISK_CONFIG="$(grep "config" "$PM2_CONFIG" | cut -d'"' -f4 | cut -d' ' -f2)" >> /dev/null
@@ -37,37 +48,36 @@ extractConfig() {
 }
 
 # This function queries the `/api/loader/status/sync` endpoint
-# and extracts the height for evaluation
-
+# and extracts the height for evaluation.
 blockMonitor() {
   BLOCK_HEIGHT="$(curl -s http://localhost:"$PORT"/api/loader/status/sync | cut -d':' -f 5 | cut -d',' -f 1)"
 }
 
 # This function terminates the lisk client at the assigned blocks
-# Preparing the installation for a cutover
-
+# preparing the installation for a cutover.
 terminateLisk() {
 	bash "$LISK_HOME/lisk.sh" stop
 }
 
-# This function downloads the new lisk client
+# Downloads the new lisk client.
 downloadLisk() {
 	wget "https://downloads.lisk.io/lisk/$BRIDGE_NETWORK/installLisk.sh"
 }
 
 # This function executes the migration of the source installation
 # and deploys the target installation, minimizing downtime.
-
 migrateLisk() {
 	bash "$(pwd)/installLisk.sh" upgrade -r "$BRIDGE_NETWORK" -d "$LISK_HOME" -0 no
 }
 
+# Sets up initial configuration and first call to the application
+# to establish baseline statistics.
+parseOption
 extractConfig
 blockMonitor
 
 # This function acts as our eventloop, keeping the process running
-# in order to monitor the node for upgrade
-
+# in order to monitor the node for upgrade.
 while [[ "$BLOCK_HEIGHT" -lt "$TARGET_HEIGHT" ]] ; do
 	blockMonitor
 	echo "$BLOCK_HEIGHT"
