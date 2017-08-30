1e4d63b3f89cfbe4833a17c3f42e2f00da55608c
isabello <isabella@lightcurve.io>
2017-08-30 15:05:56 +0200

    Add new PATH entries

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,10 +4,12 @@ cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit 2
 
 # We dont care about return code here
 # shellcheck disable=SC2155
-export PATH="$(pwd)/bin:$(pwd)/pgsql/bin:$PATH"
+export PATH="$(pwd)/bin:$(pwd)/pgsql/bin:$(pwd)/node/bin:$PATH"
 # We dont care about return code here
 # shellcheck disable=SC2155
-export LD_LIBRARY_PATH="$(pwd)/pgsql/lib:$(pwd)/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$(pwd)/pgsql/lib:$(pwd)/lib:$(pwd)/node/lib:$LD_LIBRARY_PATH"
 # We dont care about return code here
 # shellcheck disable=SC2155
-export PM2_HOME=$(pwd)/.pm2
+export PM2_HOME="$(pwd)/.pm2"
+# Node Path
+export NODE_PATH="$(pwd)/node/lib"
