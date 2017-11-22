d9f6775c6d0b09982f09c3b83d690e93f13bf894
Oliver Beddows <oliver@lightcurve.io>
2017-11-22 15:47:32 +0100

    Fix POSTGRESQL_URL

--- config.sh
+++ config.sh
@@ -26,7 +26,7 @@ NODE_CONFIG=""
 
 POSTGRESQL_DIR="postgresql-9.6.4"
 POSTGRESQL_FILE="$POSTGRESQL_DIR.tar.gz"
-POSTGRESQL_URL="https://ftp.postgresql.org/pub/source/v$POSTGRESQL_VERSION/$POSTGRESQL_FILE"
+POSTGRESQL_URL="https://ftp.postgresql.org/pub/source/v9.6.4/$POSTGRESQL_FILE"
 POSTGRESQL_OUT="pgsql"
 
 SODIUM_DIR="libsodium-1.0.11"
