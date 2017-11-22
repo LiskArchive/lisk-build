14daaf843c545446e7c5363f504b7c745e7e9cf2
Oliver Beddows <oliver@lightcurve.io>
2017-11-22 15:26:24 +0100

    Revert "Merge pull request #199 from LiskHQ/192-Restructure"
    This reverts commit 03b7727419df0237b11915864ccdcc95e4c54e55, reversing
    changes made to b05730c8236b1e19789b5c2ba0a16098e49e350f.

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,7 +3,7 @@
 cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit 2
 
 # Binary paths
-export PATH="$PWD/bin:$PWD/pgsql/bin:$PWD/node/bin:$PWD/lisky/bin:$PWD/pm2/bin:$PATH"
+export PATH="$PWD/bin:$PWD/pgsql/bin:$PWD/node/bin:$PATH"
 
 # Load dynamic libaries paths
 export LD_LIBRARY_PATH="$PWD/pgsql/lib:$PWD/lib:$PWD/node/lib:$LD_LIBRARY_PATH"
