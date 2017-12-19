4d8ead3b667d116750b68ad839483433baed6a2e
Juan Gonzalez <juan@lightcurve.io>
2017-12-19 11:42:43 +0100

    Ignore shellcheck error for sourcing nvm.sh

--- toolchains/install_linux.sh
+++ toolchains/install_linux.sh
@@ -33,6 +33,7 @@ check_cmds CMDS[@]
 
 exec_cmd "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
 export NVM_DIR="/home/$USER/.nvm"
+# shellcheck source=/dev/null
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install v6.12.2
 exec_cmd "sudo apt-get install -y autoconf automake build-essential curl chrpath git libreadline6-dev libtool zlib1g-dev libssl-dev libpq-dev nodejs python wget libncurses5-dev"
