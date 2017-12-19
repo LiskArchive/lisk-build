67421abbd8227d5c8488fb14f05a1496f2ca451c
Juan Gonzalez <juan@lightcurve.io>
2017-12-19 11:11:36 +0100

    Pull in nvm bash functions so that we can call them in install_linux.sh

--- toolchains/install_linux.sh
+++ toolchains/install_linux.sh
@@ -32,5 +32,7 @@ CMDS=("apt-get" "curl" "sudo")
 check_cmds CMDS[@]
 
 exec_cmd "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
-exec_cmd "nvm install v6.10.1"
+export NVM_DIR="/home/$USER/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install v6.12.2
 exec_cmd "sudo apt-get install -y autoconf automake build-essential curl chrpath git libreadline6-dev libtool zlib1g-dev libssl-dev libpq-dev nodejs python wget libncurses5-dev"
